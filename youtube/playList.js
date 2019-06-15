00d7e4fdef003f95dcf1fc4704f65860ffa94148
loli-000 <51862517+loli-000@users.noreply.github.com>
2019-06-16 03:00:47 +0800

    Add files via upload

---
+++ youtube/playList.js
@@ -0,0 +1,14 @@
+console.log(12345678);
+//Youtube 影片 ID
+var playList=[
+    "6V_D58qrkTc",
+    "6V_D58qrkTc",
+    "RJ2Fk0jJf5Y"
+];
+
+//播放起迄秒數
+var playTime=[
+    [140,144],
+    [192,197],
+    [24,27]
+];
